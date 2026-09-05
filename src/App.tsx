7b04b781a447fccead35a845c6758fd4be24ed03
Mikacrp29 <94135082+Mikacrp29@users.noreply.github.com>
2026-09-05 10:54:55 +0200

    retour page admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
+
 import { GroupProvider } from './contexts/GroupContext';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { AuthProvider } from './contexts/AuthContext';
+
 import { Home } from './pages/Home';
 
-const CreateGroup = lazy(() => import('./pages/CreateGroup').then((m) => ({ default: m.CreateGroup })));
-const JoinGroup = lazy(() => import('./pages/JoinGroup').then((m) => ({ default: m.JoinGroup })));
-const GroupPage = lazy(() => import('./pages/GroupPage').then((m) => ({ default: m.GroupPage })));
-const Tutorial = lazy(() => import('./pages/Tutorial').then((m) => ({ default: m.Tutorial })));
+const CreateGroup = lazy(() =>
+  import('./pages/CreateGroup').then((m) => ({
+    default: m.CreateGroup,
+  }))
+);
+
+const JoinGroup = lazy(() =>
+  import('./pages/JoinGroup').then((m) => ({
+    default: m.JoinGroup,
+  }))
+);
+
+const GroupPage = lazy(() =>
+  import('./pages/GroupPage').then((m) => ({
+    default: m.GroupPage,
+  }))
+);
+
+const Tutorial = lazy(() =>
+  import('./pages/Tutorial').then((m) => ({
+    default: m.Tutorial,
+  }))
+);
+
+const AdminPage = lazy(() =>
+  import('./pages/AdminPage').then((m) => ({
+    default: m.AdminPage,
+  }))
+);
 
 function App() {
   return (
@@ -24,6 +51,7 @@ function App() {
                 <Route path="/join/:code" element={<JoinGroup />} />
                 <Route path="/g/:code" element={<GroupPage />} />
                 <Route path="/tutoriel" element={<Tutorial />} />
+                <Route path="/admin" element={<AdminPage />} />
               </Routes>
             </Suspense>
           </GroupProvider>
